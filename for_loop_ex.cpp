74537ea599c44e5d2ce3fcfa6eadea40223197ee
ipritish <pritishjain2009@gmail.com>
2015-09-21 12:23:50 +0530

    cpp_for_loop

---
+++ for_loop_ex.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void print(int v[])
+{
+	for(int x : v)
+	{
+		cout << x << "\n";
+	}
+}
+
+void copy_fct()
+{
+	int v1[10] = {0,1,2,3,4,5,6,7,8,9};
+	int v2[10];
+	for (int i=0; i!=10 ; ++i)
+	{
+		v2[i] = v1[i];
+	}
+	print(v2);
+}
+
+int main()
+{
+	copy_fct();
+	return 0;
+}
