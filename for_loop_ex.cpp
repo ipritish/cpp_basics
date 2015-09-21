c05ab17ff01fdc5a0c4045e632000e3aabd0647f
ipritish <pritishjain2009@gmail.com>
2015-09-21 12:56:48 +0530

    for pass from ref

--- for_loop_ex.cpp
+++ for_loop_ex.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 using namespace std;
 
-void print(int v[])
+void print(int (&arr)[10])
 {
-	for(int x : v)
+	int v[] = {0,1,2,4,5};
+	for(auto x : arr)
 	{
 		cout << x << "\n";
 	}
@@ -13,7 +14,7 @@ void copy_fct()
 {
 	int v1[10] = {0,1,2,3,4,5,6,7,8,9};
 	int v2[10];
-	for (int i=0; i!=10 ; ++i)
+	for (auto i=0; i!=10 ; ++i)
 	{
 		v2[i] = v1[i];
 	}
