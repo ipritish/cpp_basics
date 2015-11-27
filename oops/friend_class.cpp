93412a0e67cb0f066d3e8dbbd38e501c2d409b56
ipritish <pritishjain2009@gmail.com>
2015-11-27 20:07:54 +0530

    friend function

---
+++ oops/friend_class.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+class Storage
+{
+private:
+    int m_nValue;
+    double m_dValue;
+public:
+    Storage(int nValue, double dValue)
+    {
+        m_nValue = nValue;
+        m_dValue = dValue;
+    }
+ 
+    // Make the Display class a friend of Storage
+    friend class Display;
+};
+ 
+class Display
+{
+private:
+    bool m_bDisplayIntFirst;
+ 
+public:
+    Display(bool bDisplayIntFirst) { m_bDisplayIntFirst = bDisplayIntFirst; }
+ 
+    void DisplayItem(Storage &cStorage)
+    {
+        if (m_bDisplayIntFirst)
+            std::cout << cStorage.m_nValue << " " << cStorage.m_dValue << std::endl;
+        else // display double first
+            std::cout << cStorage.m_dValue << " " << cStorage.m_nValue << std::endl;
+    }
+};
+
+int main()
+{
+    Storage cStorage(5, 6.7);
+    Display cDisplay(false);
+ 
+    cDisplay.DisplayItem(cStorage);
+ 
+    return 0;
+}
